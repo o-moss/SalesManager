923e3434312d04298d2f907e6c1178f140834c62
Olga Mokhova <radus888@gmail.com>
2022-08-02 18:11:40 +0300

    Trimmed mean calculation added

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,31 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+
+    public int trimmedMean() {
+        int max = this.max();
+        int min = this.min();
+        int sum = 0;
+        int trimmedMean = 0;
+        int i = 0;
+        for (int sale : sales) {
+            if (sale < max & sale > min) {
+                sum += sale;
+                i++;
+            }
+        }
+        trimmedMean = sum / i;
+        return trimmedMean;
+    }
 }
