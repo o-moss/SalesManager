ef17a3be8f14f8ae449bd66c41f5b785d88ebdd0
Olga Mokhova <radus888@gmail.com>
2022-08-02 17:09:36 +0300

    Switched int with long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] salesOfManager1 = {20, 23, 10, 3, 35};
+        long[] salesOfManager1 = {20, 23, 10, 3, 35};
         SalesManager manager1 = new SalesManager(salesOfManager1);
 
         System.out.println("Максимальные продажи Менеджера №1 за текущую неделю составили: " + manager1.max() + ".");
